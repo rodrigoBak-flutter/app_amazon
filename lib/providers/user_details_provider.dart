48cf9825033be1ec53c479e4c60264728838cab3
Mi Nombre <rodrigosebastianbak@hotmail.com>
2022-09-17 22:17:33 +0200

    app_amazon

---
+++ lib/providers/user_details_provider.dart
@@ -0,0 +1,15 @@
+import 'package:app_amazon/model/user_details_model.dart';
+import 'package:app_amazon/resources/cloudfirestore_methods.dart';
+import 'package:flutter/material.dart';
+
+class UserDetailsProvider with ChangeNotifier {
+  UserDetailsModel userDetails;
+
+  UserDetailsProvider()
+      : userDetails = UserDetailsModel(name: "Loading", address: "Loading");
+
+  Future getData() async {
+    userDetails = await CloudFirestoreClass().getNameAndAddress();
+    notifyListeners();
+  }
+}
